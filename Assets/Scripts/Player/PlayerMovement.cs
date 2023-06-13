442d16761d1fb1de861ac2ad33488ff9a4512126
Padraic Heaton <padraic.g.heaton@student.uts.edu.au>
2023-06-13 13:40:02 +1000

    added sensitivity to the settings menu

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -74,6 +74,9 @@ public class PlayerMovement : MonoBehaviour
     {
         if (CanMove)
         {
+            //! BANDAID FIX
+            sensitivity = SaveStateController.GetData<float>(SaveStateController.sensitivityValueKey);
+
             MyInput();
             Look();
         }
